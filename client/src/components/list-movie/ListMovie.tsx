05ea7814800165471b76597e4bcab3af5fd52625
lephu <tui.hrv@gmail.com>
2024-03-26 23:42:33 +0700

    add code

--- client/src/components/list-movie/ListMovie.tsx
+++ client/src/components/list-movie/ListMovie.tsx
@@ -6,21 +6,29 @@ import { Drawer, DrawerContent, DrawerTrigger } from "../ui/drawer";
 import { cn } from "@/lib/utils";
 import Showtime from "../show-time/Showtime";
 import Trailer from "../trailer/Trailer";
-import { useGetMoviesByStatusQuery } from "@/services/movies/movies.services";
+import { useGetMoviesByStatusQuery, useGetMoviesQuery } from "@/services/movies/movies.services";
 import { Movie } from "@/services/movies/movies.interface";
+import { log } from "console";
 
 type Props = {};
 
 const ListMovie = (props: Props) => {
     const [activeTab, setActiveTab] = useState("PHIM SẮP CHIẾU");
     const [modal, setModal] = useState("");
+    const [idMovie, setIdMovie] = useState<any>();
     const [status, setStatus] = useState('Phim sắp chiếu')
     const handleGetMovie = (status: string, active: string) => {
         setActiveTab(active)
         setStatus(status)
     }
+    const handleTrailer = (id: any) => {
+        setModal('trailer');
+        setIdMovie(id);
+    }
     const { data: movies } = useGetMoviesByStatusQuery(status)
-    
+
+    const { data: movie } = useGetMoviesQuery(idMovie)
+
     return (
         <Drawer>
             <div className="list-movies">
@@ -58,45 +66,44 @@ const ListMovie = (props: Props) => {
                 {/* list movie */}
                 <div className="list-movie flex ">
                     {movies?.data?.map((movie: Movie) => (
-
-                    <div className="movie-item" key={movie.id}>
-                        <DrawerTrigger className={cn('p-0 ')}>
-                            <div className={cn('movie-item__image ') }>
-                                <img
-                                    src={movie.image}
-                                    alt=""
-                                />
-                                <div
-                                    onClick={() => setModal("trailer")}
-                                    className="movie-item__overlay flex items-center justify-center"
-                                >
-                                    <div className="play-icon flex items-center justify-center">
-                                        <FaPlayCircle className="icon" />
+                        <div className="movie-item" key={movie.id}>
+                            <DrawerTrigger className={cn('p-0 ')}>
+                                <div className={cn('movie-item__image ') }>
+                                    <img
+                                        src={movie.image}
+                                        alt=""
+                                    />
+                                    <div
+                                        onClick={(): void => handleTrailer(movie.id)}
+                                        className="movie-item__overlay flex items-center justify-center"
+                                    >
+                                        <div className="play-icon flex items-center justify-center">
+                                            <FaPlayCircle className="icon" />
+                                        </div>
                                     </div>
                                 </div>
-                            </div>
-                        </DrawerTrigger>
-                        <div className="movie-item__bottom">
-                            <Link className="movie-item__title" to={`/movie/${movie.id}`}>
-                                {movie.name}
-                            </Link>
-                            <p>
-                                <strong>Thể loại:</strong>{movie.detail.categories.map((category) => category.name).join(', ')}
-                            </p>
-                            <p>
-                                <strong>Thời lượng:</strong> {movie.time} phút
-                            </p>
-                            <DrawerTrigger
-                                onClick={() => setModal("showtime")}
-                                className={cn("btn-ticket")}
-                            >
-                                mua vé
-                                <div className="icon-ticket flex items-center justify-center">
-                                    <FaTicketAlt className="icon" />
-                                </div>
                             </DrawerTrigger>
+                            <div className="movie-item__bottom">
+                                <Link className="movie-item__title" to={`/movie/${movie.id}`}>
+                                    {movie.name}
+                                </Link>
+                                <p>
+                                    <strong>Thể loại:</strong>{movie.detail.categories.map((category) => category.name).join(', ')}
+                                </p>
+                                <p>
+                                    <strong>Thời lượng:</strong> {movie.time} phút
+                                </p>
+                                <DrawerTrigger
+                                    onClick={() => setModal("showtime")}
+                                    className={cn("btn-ticket")}
+                                >
+                                    mua vé
+                                    <div className="icon-ticket flex items-center justify-center">
+                                        <FaTicketAlt className="icon" />
+                                    </div>
+                                </DrawerTrigger>
+                            </div>
                         </div>
-                    </div>
                     ))}
                 </div>
             </div>
@@ -114,7 +121,7 @@ const ListMovie = (props: Props) => {
                         "mx-auto top-[18%] fixed max-w-[50%] w-full transform  max-h-[530px] overflow-hidden"
                     )}
                 >
-                    <Trailer />
+                    <Trailer trailer={movie?.data?.trailer?.url} name={movie?.data?.name} />
                 </DrawerContent>
             )}
            
--- server/app/Http/Resources/MoviesResource.php
+++ server/app/Http/Resources/MoviesResource.php
@@ -2,6 +2,12 @@
 
 namespace App\Http\Resources;
 
+use App\Models\DayMovies;
+use App\Models\Room;
+use App\Models\Seats;
+use App\Models\SeatsType;
+use App\Models\TimeShows;
+use App\Models\Trailers;
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
 
@@ -18,6 +24,67 @@ class MoviesResource extends JsonResource
         foreach ($this->categories as $category) {
             $categoryIds[] = $category['id'];
         }
+
+        // Khởi tạo mảng để lưu thông tin về các thời gian chiếu
+        $timeShowsData = [];
+        $dayMoviesData = [];
+        // Khởi tạo mảng để lưu thông tin về các thời gian chiếu
+        $seatData = [];
+
+        // Kiểm tra xem id_time có giá trị hay không trước khi lặp
+        if (isset($this->id_time)) {
+            foreach (json_decode($this->id_time, true) as $timeId) {
+                $timeShow = TimeShows::find($timeId);
+                if ($timeShow != null) {
+                    $rooms = Room::find($timeShow->id_room);
+                    $seats = Seats::where('id_time', $timeShow->id)->get();
+                    $available_seats = Seats::where([
+                        'id_time' => $timeShow->id,
+                        'seatStatus' => 'Chưa đặt'
+                    ])->count();
+                }
+                if (isset($timeShow)) {
+                    // Thêm thông tin của thời gian chiếu vào mảng
+                    foreach ($seats as $seat) {
+                        $seatType = SeatsType::find($seat->id_seatstype);
+                        if ($seatType) {
+                            $seatData[] = [
+                                'id' => $seat->id,
+                                'nameRow' => $seat->nameRow,
+                                'seatStatus' => $seat->seatStatus,
+                                'seatType' => [
+                                    'name' => $seatType->name,
+                                    'price' => $seatType->price,
+                                ],
+                            ];
+                        }
+                    }
+                    $timeShowsData[] = [
+                        'id' => $timeShow->id,
+                        'available_seats' => $available_seats,
+                        'name' => $timeShow->name,
+                        'seat_quantities' => json_decode($timeShow->seat_quantities),
+                        'room' => $rooms,
+                        'seats' => $seatData,
+                        'created_at' => $timeShow->created_at,
+                    ];
+                }
+            }
+        }
+        if ($this->id_day_movie) {
+            foreach (json_decode($this->id_day_movie, true) as $item) {
+                $day_movie = DayMovies::find($item);
+
+                if ($day_movie) {
+                    // Thêm thông tin của thời gian chiếu vào mảng
+                    $dayMoviesData[] = [
+                        'day' => $day_movie->day,
+                        'month_rank' => $day_movie->month_rank,
+                    ];
+                }
+            }
+        }
+
         return [
             'id' => $this->id,
             'name' => $this->name,
@@ -34,7 +101,9 @@ class MoviesResource extends JsonResource
                 'categories' => $this->categories,
             ],
             'image' => $this->image,
-            'id_trailer' => $this->id_trailer,
+            'trailer' => Trailers::find($this->id_trailer),
+            'day_movies' => $dayMoviesData,
+            'time_shows' => $timeShowsData, // Sử dụng mảng chứa thông tin về thời gian chiếu
             'created_at' => $this->created_at,
             'updated_at' => $this->updated_at,
         ];
