7f56e09a77825c187a9b18510b707e53d053c9ef
vantrong02 <tronggiaosu@gmail.com>
2024-03-27 15:50:50 +0700

    detailmovie

--- client/src/pages/detailMovie/DetailMoviePage.tsx
+++ client/src/pages/detailMovie/DetailMoviePage.tsx
@@ -1,21 +1,40 @@
 import React, { useState } from "react";
 import { NavLink, useParams } from "react-router-dom";
 import "./DetailMoviePage.scss";
+import { cn } from "@/lib/utils";
 import { IoIosArrowForward } from "react-icons/io";
 import { useGetMoviesQuery } from "@/services/movies/movies.services";
 import { Category } from "@/services/categories/categories.interface";
+import Trailer from "../../components/trailer/Trailer";
+import { Drawer, DrawerContent, DrawerTrigger } from "../../components/ui/drawer";
+import { FaPlayCircle } from "react-icons/fa";
+
+interface DayMovie {
+    id: number;
+    day: number;
+    month_rank: string;
+}
+
+interface TimeShow {
+    id: number,
+    available_seats: number,
+    name: string
+}
 
 const DetailMoviePage: React.FC = () => {
     const { id } = useParams()
     const {data: movie} = useGetMoviesQuery(id!)
-    const [activeTab, setActiveTab] = useState(0); // State để lưu trữ tab hiện tại đang được chọn
+    const [activeTab, setActiveTab] = useState(1); // State để lưu trữ tab hiện tại đang được chọn
+    const [modal, setModal] = useState("");
 
     // Hàm xử lý sự kiện khi tab được click
-    const handleTabClick = (index: number) => {
-        setActiveTab(index); // Cập nhật tab hiện tại
+    const handleTabClick = (id: number) => {
+        setActiveTab(id); // Cập nhật tab hiện tại
     };
+
     
     return (
+        <Drawer>
         <div className="container">
             <div className="flex items-center py-2 breadcrumb">
                 <span className="text-[23px] !text-[#333] font-thin">
@@ -32,12 +51,24 @@ const DetailMoviePage: React.FC = () => {
                 <div className="col-lg-12 col-md-12 col-12">
                     <div className="row">
                         <div className="col-lg-3 col-md-4 col-12">
-                            <img
-                                className="rounded-4"
-                                src={movie?.data.image}
-                                alt=""
-                            />
+                            <DrawerTrigger className={cn('p-0 ')}>
+                                <div className={cn('movie-item__image ') }>
+                                    <img
+                                        src={movie?.data.image}
+                                        alt=""
+                                    />
+                                    <div
+                                        onClick={() => setModal("trailer")}
+                                        className="movie-item__overlay flex items-center justify-center"
+                                    >
+                                        <div className="play-icon flex items-center justify-center">
+                                            <FaPlayCircle className="icon" />
+                                        </div>
+                                    </div>
+                                </div>
+                            </DrawerTrigger>
                         </div>
+                        
                         <div className="col-lg-9 col-md-9 col-12 content">
                             <h2 className="fw-bold title">{movie?.data.name}</h2>
                             <div className="des py-2">
@@ -90,130 +121,64 @@ const DetailMoviePage: React.FC = () => {
                 </div>
             </div>
             <ul className="nav nav-tabs dayofweek mb-4 justify-content-start">
-                <li className={activeTab === 0 ? "activeTab" : ""}>
+            {movie?.data.day_movies.map((item: DayMovie, index: number) => (
+                <li key={index} className={activeTab === item?.id ? "activeTab" : ""}>
                     <NavLink
                         to=""
-                        onClick={() => handleTabClick(0)}
+                        onClick={() => handleTabClick(item?.id )}
                         className="text-black text-decoration-none"
                     >
                         <span
                             className="text-black fw-bold"
                             style={{ fontSize: "38px" }}
                         >
-                            23
+                            {item?.day}
                         </span>
-                        /02 - T4
+                        /{item?.month_rank}
                     </NavLink>
                 </li>
-                <li className={activeTab === 1 ? "activeTab" : ""}>
-                    <NavLink
-                        to=""
-                        onClick={() => handleTabClick(1)}
-                        className="text-black text-decoration-none"
-                    >
-                        <span
-                            className="text-black fw-bold"
-                            style={{ fontSize: "38px" }}
-                        >
-                            24
-                        </span>
-                        /02 - T5
-                    </NavLink>
-                </li>
-                <li className={activeTab === 2 ? "activeTab" : ""}>
-                    <NavLink
-                        to=""
-                        onClick={() => handleTabClick(2)}
-                        className="text-black text-decoration-none"
-                    >
-                        <span
-                            className="text-black fw-bold"
-                            style={{ fontSize: "38px" }}
-                        >
-                            25
-                        </span>
-                        /02 - CN
-                    </NavLink>
-                </li>
-                <li className={activeTab === 3 ? "activeTab" : ""}>
-                    <NavLink
-                        to=""
-                        onClick={() => handleTabClick(3)}
-                        className="text-black text-decoration-none"
-                    >
-                        <span
-                            className="text-black fw-bold"
-                            style={{ fontSize: "38px" }}
-                        >
-                            26
-                        </span>
-                        /02 - T2
-                    </NavLink>
-                </li>
-                <li className={activeTab === 4 ? "activeTab" : ""}>
-                    <NavLink
-                        to=""
-                        onClick={() => handleTabClick(4)}
-                        className="text-black text-decoration-none"
-                    >
-                        <span
-                            className="text-black fw-bold"
-                            style={{ fontSize: "38px" }}
-                        >
-                            27
-                        </span>
-                        /02 - T2
-                    </NavLink>
-                </li>
-                <li className={activeTab === 5 ? "activeTab" : ""}>
-                    <NavLink
-                        to=""
-                        onClick={() => handleTabClick(5)}
-                        className="text-black text-decoration-none"
-                    >
-                        <span
-                            className="text-black fw-bold"
-                            style={{ fontSize: "38px" }}
-                        >
-                            28
-                        </span>
-                        /02 - T3
-                    </NavLink>
-                </li>
+            ))}
             </ul>
-            {activeTab === 0 && (
-                <div>
-                    <span className="subtitle">2D phụ đề</span>
-                    <ul className="list-time mt-2">
-                        <li className="item-time">
-                            <span>9:30</span>
-                            <span>164 ghế trống</span>
-                        </li>
-                        <li className="item-time">
-                            <span>9:30</span>
-                            <span>164 ghế trống</span>
-                        </li>
-                        <li className="item-time">
-                            <span>9:30</span>
-                            <span>164 ghế trống</span>
-                        </li>
-                        <li className="item-time">
-                            <span>9:30</span>
-                            <span>164 ghế trống</span>
-                        </li>
-                        <li className="item-time">
-                            <span>9:30</span>
-                            <span>164 ghế trống</span>
-                        </li>
-                    </ul>
-                </div>
-            )}
-            {activeTab === 1 && <h2>tab 1</h2>}
-            {activeTab === 2 && <h2>tab 2</h2>}
-            {activeTab === 3 && <h2>tab 3</h2>}
-            {activeTab === 4 && <h2>tab 4</h2>}
-            {activeTab === 5 && <h2>tab 5</h2>}
+            <h3 className="pl-[15px] text-[18px] text-[#333] uppercase font-normal">
+                2D Phụ đề
+            </h3>
+            <div className="flex mb-4">
+                    {movie?.data.time_shows.map((item: TimeShow, index: number) => (
+                        <div key={index} className='mr-4'>
+                            <NavLink to={`/ticket/${item?.id}`} className="px-[5px] text-[#333] cursor-pointer">
+                                <div className="bg-[#E5E5E5] text-[#333] text-center text-sm py-1">
+                                    {item?.name}
+                                </div>
+                                <span
+                                    className={cn(
+                                        "text-xs font-normal text-center px-4"
+                                    )}
+                                >
+                                    {item?.available_seats} ghế trống
+                                </span>
+                            </NavLink>
+                        </div>
+                    ))}
+            </div>
+            {modal !== "trailer" ? (
+                <DrawerContent
+                    className={cn(
+                        "mx-auto top-[18%]  fixed max-w-[1000px] w-full transform  h-[500px] overflow-hidden"
+                    )}
+                >
+                    {/* <Showtime idMovie={idMovie}/> */}
+                </DrawerContent>
+            ) : (
+                <DrawerContent
+                    className={cn(
+                        "mx-auto top-[18%] fixed max-w-[50%] w-full transform  max-h-[530px] overflow-hidden"
+                    )}
+                >
+                    <Trailer trailer={movie?.data?.trailer?.url} name={movie?.data?.name} />
+                </DrawerContent>
+                )}
         </div>
+        </Drawer>
     );
 };
 
